f19b6f554bf373e04d1ed5e6b78d79d0a098779a
mi <69121442+muhammadihabk@users.noreply.github.com>
2025-06-30 18:23:38 +0300

    feat: Implement auth

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,14 @@
 import express from 'express';
 import authController from './components/auth/auth.controller';
+import passport from 'passport';
+import { handlePassportErrors } from './config/auth/passport';
+import cookieParser from './common/middlewares/cookie-parser';
 
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser);
 app.use('/auth', authController);
+app.use(passport.authenticate('jwt', { session: false }), handlePassportErrors);
 
 export default app;
